dc907f8517b8b1a3a094ecc0786dfbdbd900f36f
M1 <25163821+m1fnbr@users.noreply.github.com>
2021-08-20 14:00:04 -0400

    SpawnActor

--- Shard/Console.h
+++ Shard/Console.h
@@ -19,6 +19,71 @@ namespace Shard
 			*ViewportConsole = params.ReturnValue;
 		}
 
+
+		static UObject* SpawnActor(UClass* Class, FVector loc, FQuat rot) {
+
+
+			struct UGameplayStatics_GetPlayerController_Params
+			{
+				class UObject* WorldContextObject;                                       // (ConstParm, Parm, ZeroConstructor, IsPlainOldData)
+				int                                                PlayerIndex;                                              // (Parm, ZeroConstructor, IsPlainOldData)
+				class APlayerController* ReturnValue;                                              // (Parm, OutParm, ZeroConstructor, ReturnParm, IsPlainOldData)
+			};
+
+			UGameplayStatics_GetPlayerController_Params params1;
+
+			params1.PlayerIndex = 0;
+			params1.WorldContextObject = (UObject*)Globals::UWorld;
+
+
+			auto getplayercontroller = Unreal::FindObjectJake(L"/Script/Engine.GameplayStatics.GetPlayerController");
+			ProcessEvent(Globals::GameplayStatics, getplayercontroller, &params1);
+
+			struct BeginSpawnActorFromClass
+			{
+				class UObject* WorldContextObject;                                       // (ConstParm, Parm, ZeroConstructor, IsPlainOldData)
+				class UClass* ActorClass;                                               // (Parm, ZeroConstructor, IsPlainOldData)
+				struct FTransform                                  SpawnTransform;                                           // (ConstParm, Parm, OutParm, ReferenceParm, IsPlainOldData)
+				bool                                               bNoCollisionFail;                                         // (Parm, ZeroConstructor, IsPlainOldData)
+				class AActor* Owner;                                                    // (Parm, ZeroConstructor, IsPlainOldData)
+				class AActor* ReturnValue;                                              // (Parm, OutParm, ZeroConstructor, ReturnParm, IsPlainOldData)
+			};
+			BeginSpawnActorFromClass params;
+			params.WorldContextObject = reinterpret_cast<UObject*>(params1.ReturnValue);
+			params.ActorClass = Class;
+
+			FTransform transform;
+			transform.Rotation = rot;
+			transform.Translation = loc;
+			transform.Scale3D = FVector{ 0,0,0 };
+			params.SpawnTransform = transform;
+			params.Owner = nullptr;
+
+			auto startspawnactor = Unreal::FindObjectJake(L"/Script/Engine.GameplayStatics.BeginSpawningActorFromClass");
+			ProcessEvent(Globals::GameplayStatics, startspawnactor, &params);
+
+
+			/// Next one
+
+			struct FinishSpawningActor
+			{
+				class AActor* Actor;                                                    // (Parm, ZeroConstructor, IsPlainOldData)
+				struct FTransform                                  SpawnTransform;                                           // (ConstParm, Parm, OutParm, ReferenceParm, IsPlainOldData)
+				class AActor* ReturnValue;                                              // (Parm, OutParm, ZeroConstructor, ReturnParm, IsPlainOldData)
+			};
+
+			auto finishspawningactor = Unreal::FindObjectJake(L"/Script/Engine.GameplayStatics.BeginSpawningActorFromClass");
+
+			FinishSpawningActor params2;
+			params2.SpawnTransform = transform;
+			params2.ReturnValue;
+			params2.Actor = params.ReturnValue;
+
+			ProcessEvent(Globals::GameplayStatics, finishspawningactor, &params2);
+
+
+		}
+
 		static void GrantCheatmanager()
 		{
 			Logger::Log("Attempting to grant CheatManager");
--- Shard/Shard.cpp
+++ Shard/Shard.cpp
@@ -29,7 +29,7 @@ namespace Shard
                 }
             }
         }
-    }
+        }
 
     void Shard::Initialize()
     {       
